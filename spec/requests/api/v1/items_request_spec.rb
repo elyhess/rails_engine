e0b7090bac1ae1f97a41486421c7dd59584519ce
Jane Greene <janegreenecasa@gmail.com>
2020-08-31 17:44:32 -0600

    added new test and routes for updating items via api

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -18,4 +18,25 @@ describe "Items API" do
     item = JSON.parse(response.body)
     expect(item["data"]["id"].to_i).to eq(id)
   end
+  it "creates an item" do
+    merchant = create(:merchant)
+    item_params = {
+                    name: 'New Thingy',
+                    description: 'New description',
+                    unit_price: 84.84,
+                    merchant_id: merchant.id
+                  }
+
+    post "/api/v1/items", params: item_params
+
+    expect(response).to be_successful
+    expect(response.status).to eq(200)
+
+    item = Item.last
+
+    expect(item.name).to eq(item_params[:name])
+    expect(item.description).to eq(item_params[:description])
+    expect(item.unit_price).to eq(item_params[:unit_price])
+    expect(item.merchant_id).to eq(merchant.id)
+  end
 end
