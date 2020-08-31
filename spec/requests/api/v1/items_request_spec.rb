10e82b0f60d31f855e88bd99812272c1705cac4e
Jane Greene <janegreenecasa@gmail.com>
2020-08-31 16:36:32 -0600

    Added test for items index and show endpoints

---
+++ spec/requests/api/v1/items_request_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+describe "Items API" do
+  it "sends a list of items" do
+    create_list(:item, 3)
+
+    get '/api/v1/items'
+
+    expect(response).to be_successful
+    items = JSON.parse(response.body)
+    expect(items.count).to eq(3)
+  end
+  it "can find an item by id" do
+    id = create(:item).id
+    get "/api/v1/items/#{id}"
+
+    expect(response).to be_successful
+    item = JSON.parse(response.body)
+    expect(item["id"]).to eq(id)
+  end
+end
