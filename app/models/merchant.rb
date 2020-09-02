d142b31f8c52edf99f8a9bccd469bb625af114e0
Jane Greene <janegreenecasa@gmail.com>
2020-09-02 17:58:02 -0600

    Added helper method for getting a merchant with most revnues limited to a quanity of merchants

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,13 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   validates :name, presence: true
+
+  def self.most_revenue(quantity)
+    joins(invoices: :items).joins(invoices: :transactions)
+    .select("merchants.*, SUM(invoice_items.unit_price * invoice_items.quantity) AS revenue")
+    .merge(Transaction.successful)
+    .group('merchants.id')
+    .order('revenue DESC')
+    .limit(quantity)
+  end
 end
